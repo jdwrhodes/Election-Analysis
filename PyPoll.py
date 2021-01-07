610e98f73cc60983cf4631ade6a271bd91e03ba4
jdwrhodes <jdw.rhodes@gmail.com>
2021-01-07 10:27:37 -0600

    Moved folders around

--- PyPoll.py
+++ PyPoll.py
@@ -12,9 +12,9 @@ import csv
 import os
 
 #Assign a variable to load a file from a path
-file_to_load = os.path.join("Resources", "election_results.csv")
+file_to_load = os.path.join("./", "election_results.csv")
 #Assign a variable to save the file to a path
-file_to_save = os.path.join("./", "election_analysis.text")
+file_to_save = os.path.join("analysis", "election_analysis.text")
 
 #%%
 #Open the election results and read the file.
